b0a892e0bc064117a11dc5a78fc6c71cefca8f8e
manishkheta321 <manish.khetan123456789@gmail.com>
2020-07-26 20:09:35 +0530

    sa

--- src/App.js
+++ src/App.js
@@ -1,17 +1,71 @@
 import React from 'react';
 import './App.css';
 import { ReactComponent } from 'react';
-import {Row,Col,Container} from 'reactstrap';
 import imag from "./chahal1.jpg";
+import imag1 from './grad.jpg'
+import {Image,Row,Col,Container} from 'react-bootstrap';
 
 const Example = (props) => {
   return (
     <Container>
-      <Row>
-        <Col xs="1"><img src={imag} rounded />
+      <hr/>
+      <Row className="bodys">
+        <Col xs="2"><Image src={imag} fluid roundedCircle />
         </Col>
-        <Col xs="11"> hi</Col>
+        <Col xs="9">
+               <span><b>The Practical Dev  </b><span className = "disab">@ThePracticalDev . Sep 10</span></span>
+               <div>Learning React? Start Small.</div>
+               <div>{"{ author :"} <span className="bluecolor"> @decide </span>{"}"} </div>
+               <br/>
+               <div className="rounded-box">
+                 <div className="yellow-box" fluid>
+                  
+                  <Row>
+                    <Col xs="2"> 
+                    <Image src={imag} fluid rounded className="image1" />
+                    </Col>
+                  </Row>
+                 
+                      <div className="text-style" fluid>
+                       Hello World!
+                     </div>
+                     <Row>
+                       <Col xs="8"> </Col>
+                       <Col >
+                            
+                            <div className="rectangle-box" >
+                            <Row>
+                              <Col xs="9" className="names">
+                              <div className="textcolor">MANISH</div>
+                              
+                              </Col>
+                              <Col className="midd" >
+                              <Image src={imag} className="newimage" />
+                              </Col>
+                            </Row>
+                            </div>
+                       </Col>
+                     </Row>
+                   
+                   
+                  </div>
+                  <Row>
+                    <Col xs="10">
+                 <div className="textbox">
+                   <b>Learning React? Start Small.</b>
+                   <div>Can't pry yourself away from the tuitorials? The cure is to make tiny little experiment apps.</div>
+                  <div className="newtext" >dev.to </div>
+                 </div>
+                 </Col>
+                 </Row>
+               </div>
+               
+               <div>
+                 ..
+               </div>
+        </Col>
       </Row>
+      <hr/>
     </Container>
     );
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import {Row,Col,Container} from 'reactstrap'
 
 ReactDOM.render( 
   <React.StrictMode >
