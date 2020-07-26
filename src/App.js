b4dd57264af771e563983dd2392915b42834b48f
manishkheta321 <manish.khetan123456789@gmail.com>
2020-07-26 12:14:49 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ReactComponent } from 'react';
+import {Row,Col,Container} from 'reactstrap';
+import imag from "./chahal1.jpg";
 
-function App() {
+const Example = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Row>
+        <Col xs="1"><img src={imag} rounded />
+        </Col>
+        <Col xs="11"> hi</Col>
+      </Row>
+    </Container>
+    );
+
+   
+   
 }
+class App extends React.Component{
+    
+    constructor(props)
+    {
+        super(props);
+    }
+    render(){
+        
+        return (
+            
+            
+                <Example/>
+            
+        );
+    }
+
+}
+    
 
 export default App;
